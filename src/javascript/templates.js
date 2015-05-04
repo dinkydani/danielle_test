1c39c5d7f862c6522f72113d2d76f581fe5eb663
dinkydani <dinkydani@googlemail.com>
2015-05-04 18:28:22 +0100

    done some stuff

--- src/javascript/templates.js
+++ src/javascript/templates.js
@@ -28,23 +28,23 @@ this["Squarecat"]["templates"]["src/templates/pricing-products.hbs"] = Handlebar
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
     + ">\n    <span class=\"cell\"><img src=\"assets/pricing_images/table_icon"
     + alias3((helpers.inc || (depth0 && depth0.inc) || alias1).call(depth0,(data && data.index),{"name":"inc","hash":{},"data":data}))
-    + ".png\" alt=\"\"></span>\n    <span class=\"cell\">"
+    + ".png\" alt=\"\"></span>\n    <span class=\"cell\">\n      <span class=\"pricing-name\">"
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "<br>"
+    + "</span><span>"
     + alias3(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"description","hash":{},"data":data}) : helper)))
-    + "</span>\n    <span class=\"cell selectable\" data-product-price="
+    + "</span>\n    </span>\n    <span class=\"cell selectable\" data-product-price="
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.basic : stack1), depth0))
-    + ">£"
+    + ">\n      <span class=\"product-price\">£"
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.basic : stack1), depth0))
-    + "</span>\n    <span class=\"cell selectable\" data-product-price="
+    + "</span>\n      <span class=\"product-price-description\">Lorem ipsum dolor</span>\n    </span>\n    <span class=\"cell selectable\" data-product-price="
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.pro : stack1), depth0))
-    + ">£"
+    + ">\n      <span class=\"product-price\">£"
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.pro : stack1), depth0))
-    + "</span>\n    <span class=\"cell selectable\" data-product-price="
+    + "</span>\n      <span class=\"product-price-description\">Lorem ipsum dolor</span>\n    </span>\n    <span class=\"cell selectable\" data-product-price="
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.ultimate : stack1), depth0))
-    + ">£"
+    + ">\n      <span class=\"product-price\">£"
     + alias3(alias4(((stack1 = (depth0 != null ? depth0.prices : depth0)) != null ? stack1.ultimate : stack1), depth0))
-    + "</span>\n  </div>\n";
+    + "</span>\n      <span class=\"product-price-description\">Lorem ipsum dolor</span>\n    </span>\n  </div>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
