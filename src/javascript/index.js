2a8ac3ac3c463481585164d3a2686a1af1106dae
dinkydani <dinkydani@googlemail.com>
2015-06-20 18:37:18 +0100

    Started adding owl carousel

---
+++ src/javascript/index.js
@@ -0,0 +1,15 @@
+$(function () {
+  
+  "use strict";
+
+  $(".testimonials-carousel").owlCarousel({
+    
+    autoPlay: true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true
+
+  });
+
+
+});
